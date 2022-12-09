0ff6663bc778a7af1cec58be6323ecd2edee6b5f
Dave Lively <davelively14@gmail.com>
2022-12-08 23:13:36 -0500

    First section complete

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,8 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack {
+                if /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/ {
+                    VStack(alignment: .leading) {
+                        Text("Turtle Rock")
+                            .font(.title)
+                        HStack {
+                            Text("Joshua Tree Natural Park")
+                            Spacer()
+                            Text("California")
+                                
+                        }
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                        
+                        Divider()
+                        
+                        Text("About Turtle Rock")
+                            .font(.title2)
+                        
+                        Text("Lorum ipsum, fado grando, libro torreto.")
+                    }
+                    .padding()
+                    
+                    Spacer()
+
+                } else {
+                    /*@START_MENU_TOKEN@*/EmptyView()/*@END_MENU_TOKEN@*/
+                }
+            }
+        }
     }
 }
 
